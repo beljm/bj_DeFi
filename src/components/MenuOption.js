7e74589e9722ba0ea6f418343852c4fe42ee10d6
이지민 <jm@ijimin-ui-Macmini.local>
2022-05-29 21:24:00 +0900

    menubar, menu, menuoption structure

---
+++ src/components/MenuOption.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class MenuOption extends Component {
+  render() {
+    return (        
+      <div >
+        <div>
+          <button>token</button>
+        </div>
+        <div>
+          <button>languege</button>
+        </div>
+        <div>
+          <button>option</button>
+        </div>
+        <div>
+          <button>connectwallet</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MenuOption;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,1 +1,15 @@
-'main'
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (        
+      <div >
+        <div>광고</div>
+        <div>소개</div>
+        <div>정보</div>
+      </div>
+    );
+  }
+}
+
+export default Main;
