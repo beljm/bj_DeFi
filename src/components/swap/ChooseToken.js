7164dc8288b2ace0d6ebd7be8b27d010651ded2c
이지민 <jm@ijimin-ui-Macmini.local>
2022-05-31 14:16:07 +0900

    add toeknswap

---
+++ src/components/swap/ChooseToken.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Choosetoken(){
+
+    return(
+        <div>
+            <div>
+                <button>토큰 로고, 이름</button>
+            </div>
+            <div>
+                <input type='text' placeholder="0.0"></input>
+            </div>            
+        </div>
+    )
+}
+
+export default Choosetoken;
--- src/components/swap/Tokenswap.js
+++ src/components/swap/Tokenswap.js
@@ -1,12 +1,20 @@
 import React from "react";
+import ChooseToken from "./ChooseToken";
 
 function Tokenswap(){
 
-    return(
-        <div>
-            토큰교환
-        </div>
-    )
+  return(
+    <div>
+      <div>
+        <ChooseToken></ChooseToken>
+      </div>
+      <button>ChangeButton</button>
+      <div>
+        <ChooseToken></ChooseToken>
+      </div>
+      slippage 2%
+    </div>
+  )
 }
 
 export default Tokenswap;
