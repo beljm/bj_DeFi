69fba07585bb458793dbfbd5873c61930157605b
이지민 <jm@ijimin-ui-Macmini.local>
2022-06-02 14:38:56 +0900

    add char image

--- src/components/swap/StockChart.js
+++ src/components/swap/StockChart.js
@@ -1,10 +1,11 @@
 import React from "react";
+import chartURL from "../../images/chart.png";
 
 function StockChart(){
 
     return(
         <div>
-            그래프
+            <img src={chartURL}></img>
         </div>
     )
 }
