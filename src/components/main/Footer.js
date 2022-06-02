a3ee5b3609e799e3141e03c6d59b3833b87b338f
이지민 <jm@ijimin-ui-Macmini.local>
2022-06-02 14:52:32 +0900

    add footer

---
+++ src/components/main/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Footer(){
+  return(
+    <div>
+      <div>
+        ABOUT
+      </div>
+      <div>
+        HELP
+      </div>
+      <div>
+        DEVELOPERS
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Advertising from '../components/main/Advertising';
 import Introduce from '../components/main/Introduce';
 import Price from '../components/main/Price';
+import Footer from '../components/main/Footer';
 
 function Main() {
   return (        
@@ -14,6 +15,9 @@ function Main() {
       </div>
       <div>
         <Price/>
+      </div>
+      <div>
+        <Footer/>
       </div>
     </div>
   );
